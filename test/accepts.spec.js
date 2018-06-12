1d6de1c40612ebfa313fded49b3542610b245e1e
Durran Jordan <durran@gmail.com>
2018-06-12 17:57:11 +0200

    COMPASS-2937: Add tests for conversion ops

--- test/accepts.spec.js
+++ test/accepts.spec.js
@@ -651,6 +651,44 @@ describe('#accepts', () => {
             '}}');
         });
       });
+
+      context('when supplying conversion operators', () => {
+        it('accepts $convert', () => {
+          accepts('{$addFields: { name: { $convert: { input: true, to: "int"}}}}');
+        });
+
+        it('accepts $toBool', () => {
+          accepts('{$addFields: { name: { $toBool: true }}}');
+        });
+
+        it('accepts $toDate', () => {
+          accepts('{$addFields: { name: { $toDate: true }}}');
+        });
+
+        it('accepts $toDecimal', () => {
+          accepts('{$addFields: { name: { $toDecimal: true }}}');
+        });
+
+        it('accepts $toDouble', () => {
+          accepts('{$addFields: { name: { $toDouble: true }}}');
+        });
+
+        it('accepts $toInt', () => {
+          accepts('{$addFields: { name: { $toInt: true }}}');
+        });
+
+        it('accepts $toLong', () => {
+          accepts('{$addFields: { name: { $toLong: true }}}');
+        });
+
+        it('accepts $toObjectId', () => {
+          accepts('{$addFields: { name: { $toObjectId: true }}}');
+        });
+
+        it('accepts $toString', () => {
+          accepts('{$addFields: { name: { $toString: true }}}');
+        });
+      });
     });
 
     describe('$sort', () => {
